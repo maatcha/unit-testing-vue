3ac54193267f41b8688f863578c48e498dadcdd9
maatcha <mattcha54@gmail.com>
2020-04-28 16:51:17 +0200

    testing units with changing props

--- tests/unit/AppHeader.spec.js
+++ tests/unit/AppHeader.spec.js
@@ -6,7 +6,7 @@ describe('AppHeader', () => {
     const wrapper = mount(AppHeader)
     expect(wrapper.find('button').isVisible()).toBe(false)
   })
-  test('if user is not logged in, do not show logout button', async () => {
+  test('if user is logged in, show logout button', async () => {
     const wrapper = mount(AppHeader)
     wrapper.setData({ loggedIn: true })
 
